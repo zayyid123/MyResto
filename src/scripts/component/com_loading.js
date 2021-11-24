592f115e9e49f1ea74bcf8b17b2836d3cce6af5f
zayyid123 <58965738+zayyid123@users.noreply.github.com>
2021-11-23 21:43:56 -0800

    Delete src directory

--- src/scripts/component/com_loading.js
+++
@@ -1,9 +0,0 @@
-import { createLoading } from '../view/templates/template-creator';
-
-class comLoading extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = createLoading();
-  }
-}
-
-customElements.define('component-loading', comLoading);
--- src/scripts/globals/api-endPoint.js
+++
@@ -1,9 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  HOME: `${CONFIG.BASE_URL}/list`,
-  POST_REVIEW: `${CONFIG.BASE_URL}/review`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}/detail/${id}`,
-};
-
-export default API_ENDPOINT;
